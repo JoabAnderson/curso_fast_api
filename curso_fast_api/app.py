98ce91a48d714501c77cac4e2c38e8b54a9a0103
JoabAnderson <newjoab@gmail.com>
2025-02-11 22:55:22 -0300

    Aula 02...

--- curso_fast_api/app.py
+++ curso_fast_api/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
+from fastapi.responses import HTMLResponse
+
 from fastapi import FastAPI
+
+from curso_fast_api.schemas import Message
 
 app = FastAPI()
 
-
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula02():
+    return """  
+        <html>
+            <head>
+                <title>Nosso olá mundo!</title>
+            </head>
+            <body>
+                <h1> Olá Mundo </h1>
+            </body>
+        </html>
+        """
